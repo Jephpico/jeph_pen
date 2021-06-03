7b6ce1f35dcef90bc31f40c4f599ea8b05973150
jephpico <amadasun.jephthah@gmail.com>
2021-06-03 22:06:03 +0100

    dbug4

--- Jeph_pen/urls.py
+++ Jeph_pen/urls.py
@@ -23,7 +23,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Homepage.as_views(template_name = 'homepage.html'), name = 'homepage'),
+    path('', views.Homepage.as_view(template_name = 'homepage.html'), name = 'homepage'),
     path('posts/', include('Blog.urls')),
     path('accounts/', include ('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
